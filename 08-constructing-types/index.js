2bce46f7efadcf0df89081db08dc27e2875c9e2b
kevin3251 <herry3251@gmail.com>
2019-05-10 11:30:41 +0800

    update index.js

--- 08-constructing-types/index.js
+++ 08-constructing-types/index.js
@@ -16,6 +16,33 @@ const fakeDatabase = {
 const userType = new graphql.GraphQLObjectType({
     name: 'User',
     fields: {
-        id: { type: graphql.GraphQLString }
+        id: { type: graphql.GraphQLString },
+        name: { type: graphql.GraphQLString }
     }
+})
+
+const queryType = new graphql.GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        user: {
+            type: userType,
+            args: {
+                id: { type: graphql.GraphQLString }
+            },
+            resolve: function (_, { id }) {
+                return fakeDatabase[id]
+            }
+        }
+    }
+})
+
+const schema = new graphql.GraphQLSchema({ query: queryType })
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema, graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log('Running a GraphQL API server at localhost:4000/graphql')
 })
